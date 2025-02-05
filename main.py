d21a9ad4e1bbf1f931dda5249c9eb1450f25c241
Alexandre Cho <alecho97@gmail.com>
2025-02-04 22:07:57 -0300

    Adjust main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from solution import Solution
 
 def main():
     sol = Solution()
-    print(sol.longestMonotonicSubarray([1,4,3,3,2]))  # 2
+    print(sol.maxAscendingSum([1,4,3,3,2]))  # 2
 
 if __name__ == "__main__":
     main()
